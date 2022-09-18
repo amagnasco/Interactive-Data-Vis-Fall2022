5e98847900f44b67bbf3c0c41af7287171ce5646
Alessandro G. Magnasco <alessandromagnasco@gmail.com>
2022-09-18 17:42:16 -0400

    third assignment

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,39 @@
-console.log('hello world');
+console.log('testing that the console works');
+
+// identify the buttons & counters on the page
+const poofButton = document.getElementById('poofButton');
+const oopsButton = document.getElementById('oopsButton');
+const poofCounter = document.getElementById('poofCounter');
+const oopsCounter = document.getElementById('oopsCounter');
+
+// generate the cloud
+const cloud = document.getElementById('cloud');
+cloud.hidden = true;
+
+// create initial values for the counters
+let poofCount = 0;
+let oopsCount = 0;
+
+// populate the counters
+poofCounter.innerText = poofCount;
+oopsCounter.innerText = oopsCount;
+
+// update counters on click
+function Poof() {
+    // increments counters
+    ++poofCount;
+    ++oopsCount;
+    // update html
+    poofCounter.innerText = poofCount;
+    oopsCounter.innerText = oopsCount;
+
+    cloud.hidden = false;
+    // vanishes after 2 seconds
+    setTimeout(()=>{
+        cloud.hidden = true;
+    }, 2000);
+};
+function Oops() {
+    oopsCount = 0;
+    oopsCounter.innerText = oopsCount;
+};
