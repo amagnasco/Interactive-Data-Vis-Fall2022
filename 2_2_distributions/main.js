a3bc8231c5ec6a8ab8c211e3c449860fe602151e
Alessandro G. Magnasco <alessandromagnasco@gmail.com>
2022-12-19 16:36:04 -0500

    updated 2.2

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -18,14 +18,16 @@ d3.csv("bg_anon.csv", d3.autoType).then(data => {
 
   /* SCALES */
   // xscale  - linear,count
-  const xScale = d3.scaleLinear()
+  const xScale = d3.scaleSqrt() // was linear, changed to sqrt to zoom in the bottom left corner.
     .domain([0, d3.max(data.map(d => d.ClassC_count))])
     .range([margin.left, width - margin.right])
+    .nice(); // cleans up the min/max
 
     // yscale - linear,count
-  const yScale = d3.scaleLinear()
+  const yScale = d3.scaleSqrt() // idem
     .domain([0, d3.max(data, d => d.RepairCount)])
     .range([height - margin.bottom, margin.top])
+    .nice(); // cleans up the min/max
 
   const colorScale = d3.scaleOrdinal()
     .domain([0,1])
@@ -53,6 +55,25 @@ d3.csv("bg_anon.csv", d3.autoType).then(data => {
     .attr("transform", `translate(${margin.left},0)`)
     .call(yAxis);
 
+  /* MAKE IT GLOW */
+  // from https://www.visualcinnamon.com/2016/06/glow-filter-d3-visualization/
+  var defs = svg.append("defs"); // creates a container that defines the gradients
+  var filter = defs.append("filter") // allows us to specify which SVG elements to glow
+    .attr("id","glow");
+  filter.append("feGaussianBlur") // apply a Gaussian blur to the element
+    .attr("stdDeviation","3.5")
+    .attr("result","coloredBlur");
+  var feMerge = filter.append("feMerge"); 
+  feMerge.append("feMergeNode")
+    .attr("in","coloredBlur");
+  feMerge.append("feMergeNode") // puts the original element on top of the blurred one for the glow
+    .attr("in","SourceGraphic");
+
+  var toGlowOrNotToGlow = function(){
+    if (d=1){return ["filter","url(#glow)"]}
+    //else {return "url(#NoGlow)"};
+  };
+
   // circles
   const dot = svg
     .selectAll("circle")
@@ -62,5 +83,6 @@ d3.csv("bg_anon.csv", d3.autoType).then(data => {
     .attr("cy", d => yScale(d.RepairCount))
     .attr("r", d => rScale(d.RepairCost))
     .attr("fill", d => colorScale(d.AEP))
+    .style(d=> toGlowOrNotToGlow(d.AEP));
 
 });
